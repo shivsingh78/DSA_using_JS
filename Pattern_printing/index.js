cc99aa5f093ad24b50331d0b28dd22c9a81a13ac
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-03-28 21:38:56 +0530

    Add HTML and JavaScript files for DSA pattern printing exercises

---
+++ Pattern_printing/index.js
@@ -0,0 +1,26 @@
+// print right angle triangle
+// function rightangleTriangle(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let pattern = "";
+
+//     for (let j = 1; j <= i; j++) {
+//       pattern += "*";
+//     }
+//     console.log(pattern);
+//   }
+// }
+// rightangleTriangle(5);
+
+// print reverse Of right-angle-triangle
+// function reverseRightangleTriangle(rows) {
+//   for (let i = rows; i >= 1; i--) {
+//     let pattern = "";
+//     for (let j = 1; j <= i; j++) {
+//       pattern += "*";
+//     }
+//     console.log(pattern);
+//   }
+// }
+// reverseRightangleTriangle(5);
+
+
