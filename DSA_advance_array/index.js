b0911caeed1cb218ecd22c9482a5996c8d0662c6
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-03-31 21:48:13 +0530

    Refactor array rotation functions and add comments for clarity

--- DSA_advance_array/index.js
+++ DSA_advance_array/index.js
@@ -1,11 +1,76 @@
-function leftRotation(arr) {
-  let copy = arr[0];
-  for (let i = 0; i < arr.length - 1; i++) {
-    arr[i] = arr[i + 1];
-  }
-  arr[arr.length - 1] = copy;
+// Rotate array from left side ;
 
-  console.log(arr);
-}
+// function leftRotation(arr) {
+//   let start = arr[0]
+//   for (let i=0; i<arr.length-1; i++) {
+//   arr[i] = arr[i+1]
+//   }
+//   arr[arr.length-1] = start
 
-leftRotation([1, 2, 3, 4, 5, 6]);
+//   console.log(arr);
+// }
+
+// leftRotation([1, 2, 3, 4, 5, 6]);
+
+// rotate array from right in k times;
+
+// function twiceRightRotate(arr, k) {
+//   k = k % arr.length;
+//   let count = 0;
+//   for (let j = 0; j < k; j++) {
+//     count++;
+//     let last = arr[arr.length - 1];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//       arr[i] = arr[i - 1];
+//     }
+//     arr[0] = last;
+//   }
+
+//   console.log(arr);
+//   console.log(count);
+// }
+// twiceRightRotate([1, 2, 3], 2);
+
+// efficent algorith for left_rotating array without using nested loop
+// function leftRotate(arr, k) {
+//   let temp = new Array(arr.length);
+
+//   for (let i = 0; i < arr.length; i++) {
+//     temp[i] = arr[(i + k) % arr.length];
+//   }
+
+//   console.log(temp);
+// }
+// leftRotate([1, 2, 3], 2);
+
+// best algo for  rotating array
+
+// function leftRotate(arr, i, j) {
+//   while (i < j) {
+//     [arr[i], arr[j]] = [arr[j], arr[i]];
+//     i++;
+//     j--;
+//   }
+// }
+// let k = 2;
+// let arr2 = [1, 2, 3, 4, 5];
+// leftRotate(arr2, 0, k - 1);
+// leftRotate(arr2, k, arr2.length - 1);
+// leftRotate(arr2, 0, arr2.length - 1);
+// console.log(arr2);
+
+// function rightRotate(arr, i, j) {
+//   while (i < j) {
+//     [arr[i], arr[j]] = [arr[j], arr[i]];
+//     i++;
+//     j--;
+//   }
+// }
+
+// let k = 2;
+// let arr3 = [1, 2, 3, 4, 5];
+// rightRotate(arr3, 0, arr3.length - 1);
+// rightRotate(arr3, 0, k - 1);
+// rightRotate(arr3, k, arr3.length - 1);
+// console.log(arr3);
+
