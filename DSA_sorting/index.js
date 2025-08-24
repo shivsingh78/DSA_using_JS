ca467853dfc6edb77b22f03b6c45cd4b0b9b74dd
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-25 01:26:51 +0530

    Refactor insertion sort implementation and improve merge sort logic; enhance code clarity and structure.

--- DSA_sorting/index.js
+++ DSA_sorting/index.js
@@ -31,18 +31,54 @@
 
 // insertion sort 
 
-function insertionSort(arr){
-    for(let i=1;i<arr.length;i++){
-        let curr=arr[i]
-        let prev=i-1;
-        while(prev>=0 && arr[prev] > curr){
-            arr[prev+1] = arr[prev]
-            prev--
+// function insertionSort(arr){
+//     for(let i=1;i<arr.length;i++){
+//         let curr=arr[i]
+//         let prev=i-1;
+//         while(prev>=0 && arr[prev] > curr){
+//             arr[prev+1] = arr[prev]
+//             prev--
 
+//         }
+//         arr[prev+1] = curr;
+//     }
+//     return arr
+// }
+// console.log(insertionSort([4,1,5,2,3]));
+
+//Merge sort
+function conqor(arr,first,mid,last){
+    let temp = new Array(last-first+1);
+    let i=first,j=mid+1,k=0
+    while(i<=mid && j<=last){
+        if(arr[i]<arr[j]){
+            temp[k++] = arr[i++]
+        } else{
+            temp[k++] = arr[j++]
         }
-        arr[prev+1] = curr;
     }
-    return arr
+    while(i<=mid){
+        temp[k++] = arr[i++]
+
+    }
+    while(j<=last){
+        temp[k++] = arr[j++]
+    }
+    let p=0,t=first;
+    while(p<temp.length){
+        arr[t++] = temp[p++]
+    }
 }
-console.log(insertionSort([4,1,5,2,3]));
+
+function divide(arr,first,last){
+    if(first >= last) return
+    let mid=Math.floor(first+(last-first)/2)
+    divide(arr,first,mid)
+    divide(arr,mid+1,last)
+    conqor(arr,first,mid,last)
+}
+let arr=[8,2,1,9,5,12,4,20]
+
+divide(arr,0,arr.length-1)
+console.log(arr);
 
