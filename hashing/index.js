0ee258c92868c5bbcfc076adfbfa27526a00b054
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-09-02 23:28:50 +0530

    Add initial HTML and JavaScript files for hashing implementation; establish project structure.

---
+++ hashing/index.js
@@ -0,0 +1,46 @@
+//hashing
+//ques. You have given an array with duplicate element in a array except one element and order of duplicate element is even .
+
+// function findDuplicateElement(arr){
+//      let set= new Set()
+//      for(let i=0;i<arr.length;i++){
+//           if(set.has(arr[i])){
+//                set.delete(arr[i])
+//           } else{
+//                set.add(arr[i])
+//           }
+//      }
+//      console.log(set);
+     
+// }
+// let arr=[10,45,18,9,9,10,45,10,10]
+// findDuplicateElement(arr)
+
+// mp in js
+// let map = new Map()
+
+// map.set("name","shiv")
+// map.set("age",18)
+// map.set("isPassed",true)
+// console.log(map);
+
+// for(let key of map.keys()){
+//      console.log(key);
+     
+// }
+
+//Find frequency of a element in a array
+// let map = new Map()
+
+// let arr=[22,11,33,44,44,10,11,11,12]
+
+// for(let i=0;i<arr.length;i++){
+//      if(map.has(arr[i])){
+//           map.set(arr[i],map.get(arr[i])+1)
+//      }
+//      else{
+//           map.set(arr[i],1)
+//      }
+// }
+// console.log(map);
+
