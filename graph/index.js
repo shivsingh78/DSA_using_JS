4fbb93dc609ee814c6c64e5c4fa3feea8e45fac6
shivsingh78 <shivsinghbaghel95@gmail.com>
2026-04-26 03:32:03 +0530

    feat: implement graph data structure with vertex and edge methods

---
+++ graph/index.js
@@ -0,0 +1,87 @@
+//create a graph
+
+class Graph {
+     constructor() {
+          this.adjList = {};
+     }
+
+     addVertex(vertex){
+          if(!this.adjList[vertex]){
+               this.adjList[vertex]=[]
+          }
+     }
+     addEdge(v1,v2) {
+          this.adjList[v1].push(v2)
+          this.adjList[v2].push(v1)
+     }
+}
+
+const graph = new Graph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+
+graph.addEdge("A", "B")
+graph.addEdge("B", "C")
+graph.addEdge("A", "D")
+graph.addEdge( "D","C")
+
+//console.log(graph.adjList)
+
+//bfs traversal :- finding shortest path in unweighted graph
+function bfs(graph,start){
+     const queue=[]
+     const visited=new Set()
+     queue.push(start)
+     visited.add(start)
+
+     while(queue.length>0){
+          const node=queue.shift()
+          console.log(node)
+          for(let neighbour of graph[node]){
+               if(!visited.has(neighbour)){
+                    queue.push(neighbour)
+                    visited.add(neighbour)
+               }
+          }
+          
+     }
+}
+// bfs(graph.adjList,Object.keys(graph.adjList)[0])
+
+//dfs approach 1 using a stack :- explore all path, find a path 
+
+function dfs(graph,start){
+const stack=[]
+const visited=new Set()
+stack.push(start)
+visited.add(start)
+while(stack.length>0){
+     const node=stack.pop()
+     console.log(node)
+     for(let neighbour of graph[node]){
+          if(!visited.has(neighbour)){
+               visited.add(neighbour)
+               stack.push(neighbour)
+          }
+     }
+
+}
+}
+dfs(graph.adjList,Object.keys(graph.adjList)[0])
+
+//dfs : Appraoch 2 using recursion 
+
+function dfsTraversal(graph,node,visited=new Set()){
+     if(visited.has(node)) return 
+     console.log(node)
+
+     visited.add(node)
+     for(let neighbour of graph[node]){
+          dfsTraversal(graph,neighbour,visited)
+     }
+
+}
+dfsTraversal(graph.adjList,Object.keys(graph.adjList)[0])
